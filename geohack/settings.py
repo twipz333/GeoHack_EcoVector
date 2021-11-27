530d4376df2bf515887c648515515a07ad08d1e0
twipz333 <53153082+twipz333@users.noreply.github.com>
2021-11-27 16:09:44 +0300

    Update settings.py

--- geohack/settings.py
+++ geohack/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,6 +84,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -131,7 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ie if Heroku server
 if 'DATABASE_URL' in os.environ:
-    import dj_database_url
     DATABASES = {'default': dj_database_url.config()}
 
 # Activate Django-Heroku.
