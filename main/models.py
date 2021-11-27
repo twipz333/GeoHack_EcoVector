d4a489f9e78cbd36617233fcb354aeea0ecdc7b7
twipz333 <53153082+twipz333@users.noreply.github.com>
2021-11-27 16:19:18 +0300

    Update models.py

--- main/models.py
+++ main/models.py
@@ -4,7 +4,7 @@ from django.db.models.expressions import Value
 # Create your models here.
 class User(models.Model):
     uid = models.CharField(max_length=20, unique=True, null=False, editable=False)
-    tags = models.CharField(default=dict)
+    tags = models.CharField(max_length=1000, editable=True, default='{}')
     tg_uid = models.CharField(max_length=9, unique=True, editable=False, null=True)
     vk_uid = models.CharField(max_length=20, unique=True, editable=False,null=True)
     username = models.CharField(max_length=15, unique=True, null=True)
